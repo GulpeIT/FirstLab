99f8298bc0a060a10435d670036cf1fd6cbdf4e2
GulpeIT <grishawork-01@mail.ru>
2023-09-21 21:27:52 +0300

    Laboratory work is completed

--- app/src/main/java/com/gulpeit/firstlab/Activity1.java
+++ app/src/main/java/com/gulpeit/firstlab/Activity1.java
@@ -2,15 +2,34 @@ package com.gulpeit.firstlab;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Activity1 extends AppCompatActivity {
+
+    Button button;
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity1);
 
+        button = (Button) findViewById(R.id.buttonEntred);
+        editText = (EditText) findViewById(R.id.editText);
 
+        View.OnClickListener oclButtonEntre = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Activity1.this, Activity2.class);
+                intent.putExtra("name", editText.getText().toString());
+                startActivity(intent);
+            }
+        };
+
+        button.setOnClickListener(oclButtonEntre);
     }
 }
--- app/src/main/java/com/gulpeit/firstlab/Activity2.java
+++ app/src/main/java/com/gulpeit/firstlab/Activity2.java
@@ -2,13 +2,40 @@ package com.gulpeit.firstlab;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Activity2 extends AppCompatActivity {
 
+    TextView textView;
+    Button buttonReturn;
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity2);
+
+        textView = (TextView) findViewById(R.id.textView);
+        buttonReturn = (Button) findViewById(R.id.buttonReturn);
+
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+
+        textView.setText(name);
+
+        View.OnClickListener oclButtonReturn = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent firstActivity = new Intent(Activity2.this, Activity1.class);
+                startActivity(firstActivity);
+            }
+        };
+
+        buttonReturn.setOnClickListener(oclButtonReturn);
     }
 }
