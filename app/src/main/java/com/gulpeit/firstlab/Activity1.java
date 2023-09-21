b211e2a5d14b273fdf0dae89255fe34a78ad08be
GulpeIT <grishawork-01@mail.ru>
2023-09-21 13:23:55 +0300

    Creating a visual layout

--- app/src/main/java/com/gulpeit/firstlab/Activity1.java
+++ app/src/main/java/com/gulpeit/firstlab/Activity1.java
@@ -9,6 +9,8 @@ public class Activity1 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity1);
+
+
     }
 }
